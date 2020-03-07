08599cc05a6c41fa47a061780b94dcf1d2cc9aa9
vbaklan <uladzimirbaklan@gmail.com>
2020-03-07 18:42:06 +0300

    Enable cross origin requests

--- src/main/java/by/psu/controllers/ClassroomController.java
+++ src/main/java/by/psu/controllers/ClassroomController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class ClassroomController {
     private final ClassroomService classroomService;
 
