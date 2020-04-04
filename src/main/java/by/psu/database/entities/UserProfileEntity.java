9d18b968c068a3e154172486784e688a73688e8d
vladimir-baklan <uladzimirbaklan.itechart@gmail.com>
2020-04-04 11:36:42 +0300

    Fix one to one mapping

--- src/main/java/by/psu/database/entities/UserProfileEntity.java
+++ src/main/java/by/psu/database/entities/UserProfileEntity.java
@@ -17,8 +17,8 @@ public class UserProfileEntity {
     @Column(name = "id", columnDefinition = "entity_key")
     private String id;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id")
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
     private UserEntity user;
 
     @Column(name = "description")
